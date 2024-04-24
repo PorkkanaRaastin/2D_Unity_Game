faf68bf24b39ab9f4eede4e7979062a4d3494f60
PorkkanaRaastin <rasmusthegamer1@gmail.com>
2024-04-24 16:18:36 +0300

    Pelimusiikki sekä ääni efektit

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,4 +1,5 @@
-    using System.Collections;
+//
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -17,7 +18,9 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float jumpForce = 14f;
 
     private enum MovementState { idle, running, jumping, falling }
-    private MovementState state = MovementState.idle;
+
+    [SerializeField] private AudioSource jumpSoundEffect;
+    
     // Start is called before the first frame update
     private void Start()
     {
@@ -35,6 +38,7 @@ public class PlayerMovement : MonoBehaviour
 
         if (Input.GetButtonDown("Jump") && IsGrounded())
         {
+            jumpSoundEffect.Play();
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }
 
